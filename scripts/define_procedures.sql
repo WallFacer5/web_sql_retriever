964168f3dd5ddbd938085aa816df2776cf3f8795
WallFacer5 <zyh9715@126.com>
2020-11-05 21:02:53 +0800

    feat: define procedures

---
+++ scripts/define_procedures.sql
@@ -0,0 +1,42 @@
+delimiter //
+
+create procedure get(tb varchar(20), k int)
+begin
+    set @stmt=concat('select v from ', tb, ' where k=', k, ';');
+    prepare stmt from @stmt;
+    execute stmt;
+    deallocate prepare stmt;
+end //
+
+create procedure put(tb varchar(20), k int, v json)
+begin
+    set @stmt=concat('select count(*) from ', tb, ' where k=', k, ' into @count;');
+    prepare stmt from @stmt;
+    execute stmt;
+    deallocate prepare stmt;
+    if @count>0 then
+        set @stmt=concat("select json_merge_patch(v, '", v, "') from ", tb, ' where k=', k, ' into @merged_v;');
+        prepare stmt from @stmt;
+        execute stmt;
+        deallocate prepare stmt;
+        set @stmt=concat('update ', tb, " set v='", @merged_v, "' where k=", k, ';');
+        prepare stmt from @stmt;
+        execute stmt;
+        deallocate prepare stmt;
+    else
+        set @stmt=concat('insert into ', tb, " values (", k, ", '", v, "');");
+        prepare stmt from @stmt;
+        execute stmt;
+        deallocate prepare stmt;
+    end if;
+end //
+
+create procedure del(tb varchar(20), k int)
+begin
+    set @stmt=concat('delete from ', tb, ' where k=', k, ';');
+    prepare stmt from @stmt;
+    execute stmt;
+    deallocate prepare stmt;
+end //
+
+delimiter ;
